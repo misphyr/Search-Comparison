45d1f922d422662f3ec6d4d3412fab4064a52b21
misphyr <felipe@wvieira.com.br>
2023-05-04 20:39:17 -0300

    alteracao

--- src/Jogador.java
+++ src/Jogador.java
@@ -6,9 +6,18 @@ public class Jogador {
 	//Já que passei os nFs para cá, colocar a função que os usa aqui tbm
 	int tam = 5;
 	int[][] car = new int[tam][tam];	
+	
 	public int[] notFound = new int[100]; 
 	public int nF = 0;
-	
+	public int count=0;
+	public int[] vet;
+
+	Jogador(int[] vet) {
+		this.vet = vet;
+		Cartela();
+		BuscaFeitaPorFa(this.vet);
+		ExibirResultado();
+	}
 	//Cartela do jogador
 	//Ferramenta principal
 	public int[][] Cartela() {
@@ -31,6 +40,43 @@ public class Jogador {
 			System.out.println("\n");
 		}
 	}
+	
+	public void ExibirResultado() {
+		System.out.println("\n");
+		System.out.println("Contador = " + count);
+		System.out.println("\n");
+		ExibirCartela();
+		
+		System.out.println("Quantidade de números não encontrados: "+ nF);
+		int a = 0;
+
+		System.out.println("\nNúmeros não encontrados: ");
+		while(a < nF && nF > 0) {
+		System.out.print(notFound[a] + " | ");
+		a++;
+		}
+		}
+	
+	//Busca temporaria, precisa da sequencial e da binária
+	public void BuscaFeitaPorFa(int[] vet) {
+		for(int i = 0; i < tam; i++) {
+			for(int j = 0; j < tam; j++) {
+				for(int a = 0; a < vet.length; a++) {
+					count++;
+					
+					if(car[i][j] == vet[a]) {
+						break;
+					}
+					
+					if((count / 100) != i && (count % 100) == 0) {
+						notFound[nF] = car[i][j];
+						nF++;
+					}
+				}
+			}
+	}
+	}
+
 	
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.Random;
 
 public class Main {
 
-	public static int count=0;
-	public static int sub = -1;
-	
 
 	//código bagunçado, será organizado
 	public static void main(String[] args) {
@@ -18,45 +15,17 @@ public class Main {
 			}
 			*/
 		}
-		Jogador j1 = new Jogador();
-		j1.Cartela();
-		BuscaFeitaPorFa(vet,j1);
-		System.out.println("Contador = " + count + "\nNúmero da quebra = " + sub);
-		System.out.println("\n");
-		j1.ExibirCartela();
+		Jogador j1 = new Jogador(vet);
 		
-		System.out.println("Quantidade de números não encontrados: "+ j1.nF);
-		int a = 0;
-
-		System.out.println("\nNúmeros não encontrados: ");
-		while(a < j1.nF && j1.nF > 0) {
-		System.out.print(j1.notFound[a] + " | ");
-		a++;
-		}
+		Jogador j2 = new Jogador(vet);
+		
+		Jogador j3 = new Jogador(vet);
+		
+		Jogador j4 = new Jogador(vet);
 	}
 
-	//Busca temporaria, precisa da sequencial e da binária
-	public static void BuscaFeitaPorFa(int[] vet, Jogador j0) {
-		boolean p = false;
-		
-		for(int i = 0; i < j0.tam; i++) {
-			for(int j = 0; j < j0.tam; j++) {
-				for(int a = 0; a < vet.length; a++) {
-					count++;
-					
-					if(j0.car[i][j] == vet[a]) {
-						sub = vet[a];
-						p = false;
-						break;
-					}
-					
-					if((count / 100) != i && (count % 100) == 0) {
-						j0.notFound[j0.nF] = j0.car[i][j];
-						j0.nF++;
-					}
-				}
-			}
-	}
-	}
+	
+
+	
 }
 
